905e1058447d5e463698fe6367d9f7c61f499475
Hanbee Lee <bee1162@naver.com>
2023-07-16 14:43:20 +0900

    feat:초기설정

---
+++ src/main/java/clutch/clutchserver/ClutchserverApplication.java
@@ -0,0 +1,13 @@
+package clutch.clutchserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClutchserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClutchserverApplication.class, args);
+	}
+
+}
