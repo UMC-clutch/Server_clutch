6bab20c15ec9f48c4358caa4dbf8a0ff699df925
Hanbee Lee <bee1162@naver.com>
2023-07-24 03:53:20 +0900

    fix:roles

--- src/main/java/clutch/clutchserver/user/enums/Roles.java
+++ src/main/java/clutch/clutchserver/user/enums/Roles.java
@@ -1,4 +1,12 @@
 package clutch.clutchserver.user.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum Roles {
+    ROLE_GUEST("게스트"), ROLE_USER("사용자");
+    private String description;
+    Roles(String description){
+        this.description=description;
+    }
 }
