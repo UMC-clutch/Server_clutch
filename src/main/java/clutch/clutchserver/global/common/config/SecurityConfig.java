1d3f7bd29d382230ef4ad6237e88dc5b2f72e133
Hanbee Lee <bee1162@naver.com>
2023-07-23 23:12:31 +0900

    fix:원래대로 복구

--- src/main/java/clutch/clutchserver/global/common/config/SecurityConfig.java
+++ src/main/java/clutch/clutchserver/global/common/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package clutch.clutchserver.global.common.config;
 
-import clutch.clutchserver.global.jwt.JwtAuthenticationFilter;
-import clutch.clutchserver.global.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +7,6 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
@@ -20,7 +17,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 )
 public class SecurityConfig {
 
-    private final JwtTokenProvider jwtTokenProvider;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -38,20 +34,6 @@ public class SecurityConfig {
 
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.NEVER);
-        http.authorizeRequests()
-                .requestMatchers("/",
-                        "/error",
-                        "/favicon.ico",
-                        "/h2-console/**").permitAll()
-                .requestMatchers("/api/auth/**").permitAll()
-                .requestMatchers("/api/success").permitAll()
-                .requestMatchers("/api/**").authenticated();
-
-        http.logout()
-                .logoutUrl("/logout")
-                .clearAuthentication(true);
-
-        http.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
