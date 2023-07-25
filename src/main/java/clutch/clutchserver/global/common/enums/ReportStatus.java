c1238adacc0bc359f8dcc1955e966873e9adce72
JJadeYoon <yess9904@naver.com>
2023-07-25 19:13:34 +0900

    Feat: 신고접수 entity, repository 추가

---
+++ src/main/java/clutch/clutchserver/global/common/enums/ReportStatus.java
@@ -0,0 +1,6 @@
+package clutch.clutchserver.global.common.enums;
+
+public enum ReportStatus {
+
+    COMP, DECISIONING
+}
---
+++ src/main/java/clutch/clutchserver/report/controller/ReportController.java
@@ -0,0 +1,8 @@
+package clutch.clutchserver.report.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReportController {
+
+}
