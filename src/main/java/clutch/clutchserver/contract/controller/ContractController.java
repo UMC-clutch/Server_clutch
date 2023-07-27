b0403d4c98277ab676bd666d14d1650976f8ec8d
Hanbee Lee <bee1162@naver.com>
2023-07-27 15:55:57 +0900

    feat:controller작성

---
+++ src/main/java/clutch/clutchserver/contract/controller/ContractController.java
@@ -0,0 +1,21 @@
+package clutch.clutchserver.contract.controller;
+
+
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+@Slf4j
+public class ContractController {
+
+    @PostMapping("/contract")
+    @SecurityRequirement(name = "access-token")
+
+
+}
---
+++ src/main/java/clutch/clutchserver/contract/dto/ContractRequestDto.java
@@ -0,0 +1,9 @@
+package clutch.clutchserver.contract.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class ContractRequestDto {
+}
