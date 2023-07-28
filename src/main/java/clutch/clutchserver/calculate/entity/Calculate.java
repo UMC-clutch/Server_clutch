a0f4b55505f94ab68e9fba8458de67a9e6c0e29f
choeun7 <short0720@gmail.com>
2023-07-28 18:11:15 +0900

    feat: 사기 위험성 계산 api 구현

--- src/main/java/clutch/clutchserver/calculate/entity/Calculate.java
+++ src/main/java/clutch/clutchserver/calculate/entity/Calculate.java
@@ -4,9 +4,12 @@ import clutch.clutchserver.building.entity.Building;
 import clutch.clutchserver.global.common.BaseDateEntity;
 import clutch.clutchserver.user.entity.User;
 import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
 
 @Entity
 @Table(name = "calculate")
+@Getter
 public class Calculate extends BaseDateEntity {
 
     @Id
@@ -24,5 +27,13 @@ public class Calculate extends BaseDateEntity {
     private int deposit; // 보증금
     private boolean hasDanger; // 위험여부
 
+    @Builder
+    public Calculate(Long id, User user, Building building, int deposit, boolean hasDanger) {
+        this.id = id;
+        this.user = user;
+        this.building = building;
+        this.deposit = deposit;
+        this.hasDanger = hasDanger;
+    }
 
 }
