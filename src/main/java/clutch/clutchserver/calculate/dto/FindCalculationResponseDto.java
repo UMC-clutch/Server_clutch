85c54c808309baa176aeab31f8aca9ca582e9367
choeun7 <short0720@gmail.com>
2023-08-02 20:06:19 +0900

    fix: 위험도 계산 API response에 시세, 건물명 추가

--- src/main/java/clutch/clutchserver/calculate/dto/FindCalculationResponseDto.java
+++ src/main/java/clutch/clutchserver/calculate/dto/FindCalculationResponseDto.java
@@ -18,6 +18,9 @@ public class FindCalculationResponseDto {
     @Schema(description="주소 ID",example="1")
     private Long addressId;
 
+    @Schema(description="빌딩 이름",example="서서울삼성")
+    private String buildingName;
+
     @Schema(description="주소",example="경기도 고양시 일산서구 일산3동")
     private String address;
 
@@ -26,6 +29,9 @@ public class FindCalculationResponseDto {
 
     @Schema(description="호",example="606")
     private String ho;
+
+    @Schema(description="시세",example="2875000000")
+    private BigInteger price;
 
     @Schema(description="근저당액",example="20000000")
     private BigInteger collateralMoney;
@@ -37,13 +43,15 @@ public class FindCalculationResponseDto {
     private Boolean isDangerous;
 
     @Builder
-    public FindCalculationResponseDto(Long id, Long buildingId, Long addressId, String address, String dong, String ho,BigInteger collateralMoney, BigInteger deposit, Boolean isDangerous) {
+    public FindCalculationResponseDto(Long id, Long buildingId, Long addressId, String buildingName, String address, String dong, String ho, BigInteger price, BigInteger collateralMoney, BigInteger deposit, Boolean isDangerous) {
         this.id = id;
         this.buildingId = buildingId;
         this.addressId = addressId;
+        this.buildingName = buildingName;
         this.address = address;
         this.dong = dong;
         this.ho = ho;
+        this.price = price;
         this.collateralMoney = collateralMoney;
         this.deposit = deposit;
         this.isDangerous = isDangerous;
