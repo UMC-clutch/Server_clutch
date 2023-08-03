da6c74d284f227335bd34fbabcbe98eedce701b9
choeun7 <short0720@gmail.com>
2023-08-03 14:37:24 +0900

    fix

--- src/main/java/clutch/clutchserver/HelloController.java
+++ src/main/java/clutch/clutchserver/HelloController.java
@@ -1,7 +1,13 @@
 package clutch.clutchserver;
 
+import clutch.clutchserver.user.dto.FindUserResponseDto;
 import clutch.clutchserver.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -13,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 @AllArgsConstructor
+@Tag(name = "User", description = "유저과 관련된 API")
 public class HelloController {
     private final UserService userService;
 
@@ -25,6 +32,8 @@ public class HelloController {
     //유저 조회
     @GetMapping("/api/users")
     @SecurityRequirement(name = "access-token")
+    @Operation(summary = "유저 조회", description = "현재 유저의 정보를 반환합니다.")
+    @ApiResponse(responseCode = "200", description = "Successful Operation", content = @Content(mediaType = "application/json", schema = @Schema(implementation = FindUserResponseDto.class)))
     public ResponseEntity<?> findUser() {
         // 현재 토큰을 사용중인 유저 조회
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
