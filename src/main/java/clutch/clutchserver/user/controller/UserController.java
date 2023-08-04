7dcd5ec2c7a9b4455c8d97e954e63ddd8ed1d3ff
Hanbee Lee <bee1162@naver.com>
2023-08-04 13:04:59 +0900

    feat:전호번호 추가

--- src/main/java/clutch/clutchserver/user/controller/UserController.java
+++ src/main/java/clutch/clutchserver/user/controller/UserController.java
@@ -2,6 +2,9 @@ package clutch.clutchserver.user.controller;
 
 import clutch.clutchserver.global.common.enums.Reason;
 import clutch.clutchserver.user.dto.FindUserResponseDto;
+import clutch.clutchserver.user.dto.PhoneNumberRequestDto;
+import clutch.clutchserver.user.entity.User;
+import clutch.clutchserver.user.repository.UserRepository;
 import clutch.clutchserver.user.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -27,6 +30,7 @@ import java.util.Objects;
 public class UserController {
 
     private final UserService userService;
+    private final UserRepository userRepository;
 
     // 회원탈퇴
     @DeleteMapping("/v1/users")
@@ -58,5 +62,31 @@ public class UserController {
         String useremail = authentication.getName();    // 해당 유저의 email 조회(getName()은 이메일 조회 의미)
 
         return userService.findUser(useremail);
+    }
+
+    @PostMapping("/v1/phone-number")
+    @SecurityRequirement(name="access-token")
+    @Operation(summary = "전화번호 추가", description = "유저의 전화번호를 저장합니다")
+    @ApiResponse(responseCode = "200", description = "Successful Operation")
+    public ResponseEntity<String> addPhoneNumber(@RequestBody PhoneNumberRequestDto request) {
+        // 요청에서 전화번호를 가져옵니다.
+        String phoneNumber = request.getPhonenumber();
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String useremail = authentication.getName();
+
+        User user = userRepository.findByEmail(useremail).orElse(null);
+
+        // 유효성 검사를 진행 (생략)
+
+        // 사용자 정보를 업데이트합니다.
+        try {
+           user.updatePhoneNum(phoneNumber);
+            return ResponseEntity.ok("전화번호가 성공적으로 업데이트되었습니다.");
+        } catch (Exception e) {
+            // 예외 처리 (생략)
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("전화번호 업데이트 중 오류가 발생하였습니다.");
+        }
     }
 }
