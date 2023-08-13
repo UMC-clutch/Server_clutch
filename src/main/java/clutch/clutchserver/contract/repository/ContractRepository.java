d40817affa94710b10f2d6fa05f8bc7a7e80acab
윤종석 <113321706+JJadeYoon@users.noreply.github.com>
2023-08-13 19:08:29 +0900

    Revert "fix: 신고 접수 조회 #75"

--- src/main/java/clutch/clutchserver/contract/repository/ContractRepository.java
+++ src/main/java/clutch/clutchserver/contract/repository/ContractRepository.java
@@ -3,8 +3,6 @@ package clutch.clutchserver.contract.repository;
 import clutch.clutchserver.contract.entity.Contract;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public  interface ContractRepository extends JpaRepository<Contract,Long> {
-    Optional<Contract> findByUserId(Long userId);
+    Contract findByUserId(Long userId);
 }
